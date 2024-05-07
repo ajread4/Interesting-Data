cc4e9038ca0bef4a4001e4d13f110fb7e832a787
ajread4 <read.austin@gmail..com>
2024-05-07 11:03:01 -0400

    added more patterns and set up counter of files

--- Interesting-Data.psm1
+++ Interesting-Data.psm1
@@ -47,12 +47,12 @@ Finds all files with extension .txt on \\172.20.20.12\test that contain the word
         [Parameter(Position = 2, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
         [ValidateNotNullOrEmpty()]
         [String[]]
-        $Extensions=('*.txt','*.md','*.doc','*.xlsx'),
+        $Extensions=('*.txt','*.md','*.doc','*.xlsx','*.csv','*.pptx','*.sh','*.config','*.json','*.yaml','*.ssh'),
 
         [Parameter(Position = 3, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
         [ValidateNotNullOrEmpty()]
         [String[]]
-        $Patterns='creds|password',
+        $Patterns='creds|password|username|credentials|pass|login|user|pwd|passcode|passphrase|secret|token|key|login|signin|auth|authenticate|authorization|access|pin|security code|access code|encrypted|decryption|secure|unlock|protected|confidential|verification|verify|api_key|private_key|public_key|encryption_key|passphrase|account|user_id|username|security_question|recovery_question|master_password|root_password|admin_password|backup_password|secret_key|entry_code|ssl_cert|ssl_key|auth_token|cipher|crypt|hash',
 
         [bool] $Names=$False,
         [bool] $Content=$False,
@@ -70,12 +70,12 @@ Finds all files with extension .txt on \\172.20.20.12\test that contain the word
             Write-Host "-------------------------------------"
 
             $PatternFiles = Get-ChildItem -Path \\$Share\$Drive -Include $Extensions -Recurse -Name -Force
+            Write-Host "Number of Files To Parse" $PatternFiles.Count
             foreach($PatternFile in $PatternFiles){
                 if ($Names){ <# Retrieve files with only specific extensions#>
                     Write-Host $PatternFile
                 }
                 elseif($Content){ <# Retrieve files with possible credential patterns within specific extensions#>
-                    
                     $Results = Get-Content -Path \\$Share\$Drive\$PatternFile | Select-String -Pattern $Patterns
 
                     if ($Results -ne $null){
